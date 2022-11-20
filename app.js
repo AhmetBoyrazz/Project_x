6c37357d6829b089ef2ad6ef56242f6f54145bb7
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-20 21:11:35 +0100

    functions

--- app.js
+++ app.js
@@ -156,3 +156,101 @@ Grid.prototype.getColumnIndices = function (index) {
     }
     return column;
 };
+
+// get diagonal cells
+// arg 0: from top-left
+// arg 1: from top-right
+Grid.prototype.getDiagValues = function (arg) {
+    var cells = [];
+    if (arg !== 1 && arg !== 0) {
+        console.error("Wrong arg for getDiagValues!");
+        return undefined;
+    } else if (arg === 0) {
+        cells.push(this.cells[0]);
+        cells.push(this.cells[4]);
+        cells.push(this.cells[8]);
+    } else {
+        cells.push(this.cells[2]);
+        cells.push(this.cells[4]);
+        cells.push(this.cells[6]);
+    }
+    return cells;
+};
+
+// get diagonal cells
+// arg 0: from top-left
+// arg 1: from top-right
+Grid.prototype.getDiagIndices = function (arg) {
+    if (arg !== 1 && arg !== 0) {
+        console.error("Wrong arg for getDiagIndices!");
+        return undefined;
+    } else if (arg === 0) {
+        return [0, 4, 8];
+    } else {
+        return [2, 4, 6];
+    }
+};
+
+// Get first index with two in a row (accepts computer or player as argument)
+Grid.prototype.getFirstWithTwoInARow = function (agent) {
+    if (agent !== computer && agent !== player) {
+        console.error("Function getFirstWithTwoInARow accepts only player or computer as argument.");
+        return undefined;
+    }
+    var sum = agent * 2,
+        freeCells = shuffleArray(this.getFreeCellIndices());
+    for (var i = 0; i < freeCells.length; i++) {
+        for (var j = 0; j < 3; j++) {
+            var rowV = this.getRowValues(j);
+            var rowI = this.getRowIndices(j);
+            var colV = this.getColumnValues(j);
+            var colI = this.getColumnIndices(j);
+            if (sumArray(rowV) == sum && isInArray(freeCells[i], rowI)) {
+                return freeCells[i];
+            } else if (sumArray(colV) == sum && isInArray(freeCells[i], colI)) {
+                return freeCells[i];
+            }
+        }
+        for (j = 0; j < 2; j++) {
+            var diagV = this.getDiagValues(j);
+            var diagI = this.getDiagIndices(j);
+            if (sumArray(diagV) == sum && isInArray(freeCells[i], diagI)) {
+                return freeCells[i];
+            }
+        }
+    }
+    return false;
+};
+
+Grid.prototype.reset = function () {
+    for (var i = 0; i < this.cells.length; i++) {
+        this.cells[i] = 0;
+    }
+    return true;
+};
+
+//==================================
+// MAIN FUNCTIONS
+//==================================
+
+// executed when the page loads
+function initialize() {
+    myGrid = new Grid();
+    moves = 0;
+    winner = 0;
+    gameOver = false;
+    whoseTurn = player; // default, this may change
+    for (var i = 0; i <= myGrid.cells.length - 1; i++) {
+        myGrid.cells[i] = 0;
+    }
+    // setTimeout(assignRoles, 500);
+    setTimeout(showOptions, 500);
+    // debugger;
+}
+
+// Ask player if they want to play as X or O. X goes first.
+function assignRoles() {
+    askUser("Do you want to go first?");
+    document.getElementById("yesBtn").addEventListener("click", makePlayerX);
+    document.getElementById("noBtn").addEventListener("click", makePlayerO);
+}
